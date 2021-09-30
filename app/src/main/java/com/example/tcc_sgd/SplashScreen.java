db4042e1c8d6550ad61de75c2ecedf5e092e1c9f
SGD <kaiquesenai2020@gmail.com>
2021-09-30 09:10:11 -0300

    Splash Screen realizada com sucesso!

---
+++ app/src/main/java/com/example/tcc_sgd/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.tcc_sgd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
