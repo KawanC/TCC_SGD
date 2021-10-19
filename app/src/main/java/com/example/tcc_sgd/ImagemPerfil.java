1b68ac112ef0d95b4d162593c8344aa8af3b9793
aluno <matheusvecordeiro@gmail.com>
2021-10-19 17:24:48 -0300

    Imagem quase pronta

---
+++ app/src/main/java/com/example/tcc_sgd/ImagemPerfil.java
@@ -0,0 +1,17 @@
+package com.example.tcc_sgd;
+
+public class ImagemPerfil {
+    String imagemPerfil;
+
+    public ImagemPerfil(String imagemPerfil) {
+        this.imagemPerfil = imagemPerfil;
+    }
+
+    public String getImagem() {
+        return imagemPerfil;
+    }
+
+    public void setImagem(String imagemPerfil) {
+        this.imagemPerfil = imagemPerfil;
+    }
+}
--- app/src/main/java/com/example/tcc_sgd/PerfilFragment.java
+++ app/src/main/java/com/example/tcc_sgd/PerfilFragment.java
@@ -2,8 +2,11 @@ package com.example.tcc_sgd;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
@@ -13,26 +16,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PerfilFragment extends Fragment {
 
-    private TextView email, nome, nomeTela, emailTela, telefone, data_nasc;
-    private Button deslogar;
+    private TextView email, nome, telefone, data_nasc;
+    private Button deslogar, atualizar;
+    CircleImageView imagemPerfil;
     View view;
     FirebaseFirestore feed = FirebaseFirestore.getInstance();
+    FirebaseDatabase imagem = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = imagem.getReference();
+    FirebaseStorage storage = FirebaseStorage.getInstance();
     String usuarioID;
     AlertDialog.Builder builderDialog;
     AlertDialog alertDialog;
+
+    String emailString;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,6 +72,42 @@ public class PerfilFragment extends Fragment {
        telefone = view.findViewById(R.id.textViewtelefone);
        data_nasc = view.findViewById(R.id.textViewDataNascimento);
         deslogar = view.findViewById(R.id.buttonDeslogar);
+        imagemPerfil = view.findViewById(R.id.imageViewPerfil);
+        atualizar = view.findViewById(R.id.buttonAtualizarImagemPerfil);
+
+        emailString = email.getText().toString();
+
+        imagem.getReference().child("Usuario").child(nome.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+ //           ImagemPerfil img = snapshot.getValue(ImagemPerfil.class);
+   //           Glide.with(view.getContext()).load(img.getImagem()).into(imagemPerfil);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        atualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+        imagemPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 33);
+            }
+        });
 
         try {
             usuarioID = FirebaseAuth.getInstance().getCurrentUser().getEmail();
@@ -128,6 +187,42 @@ public class PerfilFragment extends Fragment {
 
         }else {
             getActivity().finish();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(data.getData() != null){
+            Uri profileUri = data.getData();
+            imagemPerfil.setImageURI(profileUri);
+
+            final StorageReference reference = storage.getReference().child("Fotos_Perfil")
+                    .child(email.getText().toString());
+
+            reference.putFile(profileUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            myRef.child("Usuario").child("imagemPerfil").setValue(uri.toString());
+
+                            Toast.makeText(getContext(), "Ur", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                }
+
+
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(view.getContext(), "Dur", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
